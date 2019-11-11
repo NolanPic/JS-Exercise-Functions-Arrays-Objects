8cdce52738f57d9e3782e527e2b63e4519c5a465
= <=>
2019-11-11 13:40:40 -0800

    Passed getOlderCars()

--- index.js
+++ index.js
@@ -246,8 +246,18 @@ function getModelYears(inventory) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(inventory, maxYear) {
+  let matches = [];
+
+  for(let i = 0; i <  inventory.length; i++) {
+
+    if(inventory[i].car_year <= maxYear) {
+      matches.push(inventory[i]);
+    }
+
+  }
+
+  return matches;
 }
 
 /**
